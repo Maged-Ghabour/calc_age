e002592eaa8e430c8ada71056630bed54b0d5cf5
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2023-06-30 13:06:09 +0300

    calulate Age

---
+++ main.js
@@ -0,0 +1,65 @@
+let calcBtn = document.getElementById('calc');
+let birthday = document.getElementById('birthday');
+birthday.value = new Date().toISOString().split('T')[0];
+birthday.max = new Date().toISOString().split('T')[0];
+let years = document.getElementById('years');
+let monthes = document.getElementById('monthes');
+let days = document.getElementById('days');
+
+
+
+calcBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  // Birthday
+
+  let d1 = new Date(birthday.value).getDate(); 
+  let m1 = new Date(birthday.value).getMonth() + 1;
+  let y1 = new Date(birthday.value).getFullYear();
+  
+
+  // Date Now
+
+  let d2 = new Date().getDate(); 
+  let m2 = new Date().getMonth() + 1;
+  let y2 = new Date().getFullYear();
+
+
+
+  let d3, m3, y3; 
+
+  y3 = y2 - y1;
+
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else { 
+    m3--;
+    d3 = getDaysInMontn(y1, m1) + d2 - d1;
+  }
+
+  if (m3 < 0) {
+    m3 = 11
+    y3--;
+  }
+
+
+  years.innerHTML = y3;
+  monthes.innerHTML = m3;
+  days.innerHTML = d3;
+
+  document.querySelector('.result').style.display = 'block'
+
+})
+
+
+function getDaysInMontn(years, months) {
+  return new Date().getDate(years,months,0)
+}
+
---
+++ notes.md
@@ -0,0 +1,15 @@
+
+## Note 1 
+input::-webkit-calendar-picker-indicator => `this refer to icon in input file `
+
+## Note 2 
+date.value = new Date().toISOString().split('T')[0]; `to make value of input file`
+
+## Note 3 
+
+date.max = new Date().toISOString().split('T')[0]; `validate max value `
+
+## Need to make 
+`Spinner to this Website `
+
+# Demo : 
